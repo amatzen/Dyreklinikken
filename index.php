6cd2b4a1ce1f287382ae8a6f58111c63befce59a
Alexander Matzen <alexander@amatzen.dk>
2018-12-10 08:37:12 +0100

    Added html structure + robots.txt

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 session_name('_sid');
 session_start();
+?>
+<!doctype html>
+<html lang="da">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dyreklinikken - Informatik projekt</title>
+</head>
+<body>
+
+</body>
+</html>
