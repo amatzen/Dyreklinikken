c4a46a56e7982a9e7a63bc2315ea3be83cdf732b
Alexander Matzen <alexander@amatzen.dk>
2019-01-04 13:15:34 +0100

    Added structure for files

--- index.php
+++ index.php
@@ -4,18 +4,27 @@ session_start();
 ?>
 <!doctype html>
 <html lang="da">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dyreklinikken - Informatik projekt</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Dyreklinikken - Informatik projekt</title>
 
-    <meta name="robots" content="noindex,nofollow">
-</head>
-<body>
+        <!-- CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+
+        <!-- JS -->
+        <script src="assets/js/jquery.js" async></script>
+        <script src="assets/js/bootstrap.js" async></script>
+
+        <meta name="robots" content="noindex,nofollow">
+    </head>
+    <body>
 
 
-</body>
+
+
+    </body>
 </html>
