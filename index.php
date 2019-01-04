e32d6171f02b22e15a7ace22eb7d0f45deb153e5
Alexander Matzen <alexander@amatzen.dk>
2019-01-04 12:47:34 +0100

    Added robots metatag

--- index.php
+++ index.php
@@ -11,8 +11,11 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dyreklinikken - Informatik projekt</title>
     <link rel="stylesheet" href="assets/css/style.css">
+
+    <meta name="robots" content="noindex,nofollow">
 </head>
 <body>
+
 
 </body>
 </html>
