d501225c084fab0d54c13907a82bd76156f1c2aa
Shimitan <mike.jensen.at@gmail.com>
2019-01-30 12:14:30 +0100

    Udskriv "Hello World"

---
+++ Test/Mike.php
@@ -0,0 +1,20 @@
+<?php
+session_name('_sid');
+session_start();
+echo "Hello World"
+?>
+<!doctype html>
+<html lang="da">
+    <head>
+   
+    </head>
+    <body>
+
+    <div id="hero">
+
+    </div>
+
+
+
+    </body>
+</html>
