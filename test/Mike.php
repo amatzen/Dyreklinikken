f4100e07b1aa27f2043048730de3dd1b1918f957
Alexander Matzen <alexander@amatzen.dk>
2019-01-30 12:23:40 +0100

    Tilføjet igen

---
+++ test/Mike.php
@@ -0,0 +1,20 @@
+<?php
+session_name('_sid');
+session_start();
+?>
+<!doctype html>
+<html lang="da">
+    <head>
+   Hello World2
+
+    </head>
+    <body>
+
+    <div id="hero">
+
+    </div>
+
+
+
+    </body>
+</html>
