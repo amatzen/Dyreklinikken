3859c81172bcb9ea27cf9718730157ecc0de13f7
Nikolaj <43805903+NikolajLang@users.noreply.github.com>
2019-01-30 12:41:13 +0100

    Update nikolaj.php

--- test/nikolaj.php
+++ test/nikolaj.php
@@ -4,18 +4,19 @@ session_start();
 ?>
 <!doctype html>
 <html lang="da">
-<title>
-    Test Nikolaj
-</title>
-    <head>
-   Hello World
+    <title>
+        Test Nikolaj
+    </title>
 
+    <head>
+        <b>Hello World</b>
     </head>
+
     <body>
 
-    <div id="hero">
+        <div id="hero">
 
-    </div>
+        </div>
 
 
 
