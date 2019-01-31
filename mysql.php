769e12ec871e10fdad3ecbde4f30228538fa990f
Alexander Matzen <alexander@amatzen.dk>
2019-01-31 11:16:14 +0100

    test

--- mysql.php
+++ mysql.php
@@ -1,5 +1,5 @@
 <?php
-$db = new mysqli("iqu.dk", "mgx_dyrek", "Ci50oh~8", "mgx_dyreklinikken");
+$db = new mysqli("infrequent.dk", "mgx_dyrek", "Ci50oh~8", "mgx_dyreklinikken");
 
 if(isset($db->connect_errno)) {
     die("Failed establishing connection to MySQL: ".$db->connect_errno. " - ".$db->connect_error);
