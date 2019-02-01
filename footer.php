3910105c39ef785f75fd08be8aafca664c072505
Alexander Matzen <alexander@amatzen.dk>
2019-02-01 09:21:49 +0100

    Footer added

--- footer.php
+++ footer.php
@@ -1,10 +1,85 @@
-<footer class="container-fluid">
-    <div class="container">
+<footer class="footer">
+    <div class="container text-center text-md-left">
         <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-3"></div>
-            <div class="col-md-3"></div>
-            <div class="col-md-3"></div>
+            <div class="col-md-9">
+                <div class="row">
+                    <div class="col-md-3 mx-auto">
+                        <img class="mt-3" height="50" src="https://i.imgur.com/Odwm52p.png">
+                    </div>
+                    <hr class="clearfix w-100 d-md-none">
+                    <div class="col-md-3 mx-auto">
+                        <h5 class="mt-3 mb-4">Dyreklinikken A/S</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a>Hospitalsgade 26</a>
+                            </li>
+                            <li>
+                                <a>2400 København V,</a>
+                            </li>
+                            <li>
+                                <a>Danmark</a>
+                            </li>
+                            <li>
+                                <br>
+                            </li>
+                            <li>
+                                <a>CVR: 40042199</a>
+                            </li>
+                            <br />
+                            <li>
+                                <b>M: </b><a href="mailto:info@dyreklinikken.dk">info@dyreklinikken.dk</a></a>
+                            </li>
+                            <li>
+                                <b>T: </b><a href="tel:+4570107070">+45 70 10 70 10</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <hr class="clearfix w-100 d-md-none">
+                    <div class="col-md-3 mx-auto">
+                        <h5 class="mt-3 mb-4">Navigation</h5>
+                        <ul class="links">
+                            <li>
+                                <a href="#!">Forside</a>
+                            </li>
+                            <li>
+                                <a href="#!">Om os</a>
+                            </li>
+                            <li>
+                                <a href="#!">Priser</a>
+                            </li>
+                            <li>
+                                <a href="#!">Booking</a>
+                            </li>
+                            <li>
+                                <a href="#!">Online Shop</a>
+                            </li>
+                            <li>
+                                <a href="#!">Sitemap</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <hr class="clearfix w-100 d-md-none">
+                    <div class="col-md-3 mx-auto">
+                        <h5 class="overskrift font-weight-bold mt-3 mb-4">Find din dyreklinik</h5>
+                        <img height="200" src="https://i.imgur.com/qZtEgJH.png">
+                    </div>
+                    <div class="col mt-3">
+                        <div class="left footer-copyright">&copy; <?= date("Y") ?> Dyreklinikken A/S - Alle rettigheder forbeholdt</div>
+                        <div class="right footer-copyright mt-2 mt-sm-0 mb-2"><a href="#!">Servicevilkår</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="#!">Privatlivspolitik</a> </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 emergency">
+                <h2>Akut?</h2>
+                <span class="emergency-text">
+                    Ring til vores hotline:
+                    <p class="emergency-phone">
+                        +45 71 71 71 71
+                    </p>
+                </span>
+            </div>
         </div>
+
     </div>
+
 </footer>
--- index.php
+++ index.php
@@ -11,13 +11,15 @@ require_once 'mysql.php';
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Dyreklinikken - Informatik projekt</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
         <!-- CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/css/bootstrap-grid.min.css">
         <link rel="stylesheet" href="assets/css/bootstrap.css">
         <link rel="stylesheet" href="assets/css/style.css">
 
         <!-- JS -->
-        <script src="assets/js/jquery.js" async></script>
+        <script src="assets/js/jquery.js"></script>
         <script src="assets/js/bootstrap.js" async></script>
 
         <meta name="robots" content="noindex,nofollow">
@@ -29,4 +31,13 @@ require_once 'mysql.php';
     <?php require_once 'footer.php'; ?>
 
     </body>
+
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/webfont/1.6.28/webfontloader.js'></script>
+    <script async>
+        WebFont.load({
+            google: {
+                families: ['Source Sans Pro:200,300,400,600,700,900', 'Slabo 13px']
+            }
+        });   
+    </script>
 </html>
